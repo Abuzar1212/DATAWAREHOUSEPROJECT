3d1e90c9b0e140475e41d64d2ccfc7d7a0fcb925
Abuzar1212 <139246414+Abuzar1212@users.noreply.github.com>
2025-05-23 21:58:05 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,18 @@
+-- Create Database 'Datawarehouse'
+
+use master;
+
+CREATE DATABASE DataWarehouse;
+
+use DataWarehouse;
+
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
+GO
+
+
+
 
